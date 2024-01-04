13df997b1c9d2f35809fa0c5db40362fe3884e2d
Himanshu Ranjan <hranjan.hr239@gmail.com>
2024-01-05 02:01:28 +0530

    First Commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import loading from './loading.gif'
+
+const Spinner = () => {
+  
+    return (
+      <div>
+          <img src="src\components\loading.gif" alt="loading"/>
+      </div>
+    )
+  
+}
+
+export default Spinner
